a3502493a706858dbd1cddb9f22037fba41676d5
Matthew En <86386184+Mcen25@users.noreply.github.com>
2025-03-15 12:03:37 -0400

    Remove content and added room glb file

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -3,23 +3,14 @@ import { Router, Route } from "@solidjs/router";
 import HomePage from "./pages/HomePage";
 import AboutPage from "./pages/AboutPage";
 import LoginPage from "./pages/LoginPage";
-import RegisterAccount from "./pages/RegisterAccount";
 import ContentPage from "./pages/ContentPage";
-import EarthPage from "./pages/content/EarthPage";
-import BlackHolePage from "./pages/content/BlackHolePage";
-import ISSPage from "./pages/content/ISSPage";
 
 function App() {
   return (
     <Router>
       <Route path="/" component={HomePage} />
       <Route path="/about" component={AboutPage} />
-      <Route path="/login" component={LoginPage} />
       <Route path="/content" component={ContentPage} />
-      <Route path="/register" component={RegisterAccount} />
-      <Route path="/earth" component={EarthPage} />
-      <Route path="/blackhole" component={BlackHolePage} />
-      <Route path="/ISS" component={ISSPage} />
     </Router>
   );
 }
--- client/src/pages/ContentPage.jsx
+++ client/src/pages/ContentPage.jsx
@@ -2,10 +2,6 @@ import { createSignal } from "solid-js";
 import styles from "./ContentPage.module.css";
 import Navbar from "../components/NavBar";
 import GLTFPage from "../components/GLTFPage.jsx";
-import blackHoleModel from "../assets/blackhole/scene.gltf";
-import shibaModel from "../assets/shiba/scene.gltf";
-import earthModel from "../assets/earth/scene.gltf";
-import earthTexture from "../assets/earth/textures/Material.002_diffuse.jpeg";
 
 export default function ContentPage() {
   return (
@@ -68,39 +64,6 @@ export default function ContentPage() {
           </div>
           <div class={styles.rightSection}>
             <div class={styles.cardGrid}>
-              <div
-                class={styles.card}
-                onClick={() => (window.location.href = "/earth")}
-              >
-                <img src={earthTexture} alt="Earth" />
-                <h3>Earth Model</h3>
-                <p>Interactive 3D Earth model</p>
-              </div>
-              <div
-                class={styles.card}
-                onClick={() => (window.location.href = "/blackhole")}
-              >
-                <img src="/blackhole-thumbnail.jpg" alt="Black Hole" />
-                <h3>Black Hole</h3>
-                <p>Astronomical visualization</p>
-              </div>
-              <div
-                class={styles.card}
-                onClick={() => (window.location.href = "/shiba")}
-              >
-                <img src="/shiba-thumbnail.jpg" alt="Shiba" />
-                <h3>Shiba Model</h3>
-                <p>3D character model</p>
-              </div>
-
-              <div
-                class={styles.card}
-                onClick={() => (window.location.href = "/ISS")}
-              >
-                <img src={earthTexture} alt="ISS" />
-                <h3>ISS Model</h3>
-                <p>Interactive 3D ISS model</p>
-              </div>
             </div>
           </div>
         </div>
