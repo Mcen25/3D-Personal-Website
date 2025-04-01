55069737f8875b1ce9973c699fc58c1e7f363bee
Matthew En <86386184+Mcen25@users.noreply.github.com>
2025-04-01 12:13:44 -0400

    remove solid base

--- client/src/pages/AboutPage.jsx
+++
@@ -1,9 +0,0 @@
-// src/pages/AboutPage.jsx
-export default function AboutPage() {
-    return (
-      <div>
-        <h1>About</h1>
-        <p>This is the about page.</p>
-      </div>
-    );
-  }
