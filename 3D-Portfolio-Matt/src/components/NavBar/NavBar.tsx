1d207493ebc0e421041a78a2c2d6d0d425295156
Matthew En <86386184+Mcen25@users.noreply.github.com>
2025-04-03 18:16:57 -0400

    Navbar and footer added

--- 3D-Portfolio-Matt/src/components/NavBar/NavBar.tsx
+++ 3D-Portfolio-Matt/src/components/NavBar/NavBar.tsx
@@ -10,63 +10,36 @@ const navigation = [
   { name: 'Resume', href: 'https://drive.google.com/file/d/1ltXc6CbdqCk--ctWWygkw-YYVQXiEgKP/view?usp=sharing', current: false },
 ];
 
-function classNames(...classes: string[]) {
-  return classes.filter(Boolean).join(' ');
-}
-
 const NavBar: FC<NavBarProps> = () => {
   return (
-    <Disclosure as="nav" className="rgba(17,17,17,255)">
-      <div className="mx-auto max-w-7xl px-2 sm:px-6 lg:px-8">
-        <div className="relative flex h-16 items-center justify-between">
-          <div className="absolute inset-y-0 left-0 flex items-center sm:hidden">
-            <DisclosureButton className="group relative inline-flex items-center justify-center rounded-md p-2 text-gray-400 hover:bg-gray-700 hover:text-white focus:ring-2 focus:ring-white focus:outline-none focus:ring-inset">
-              <span className="absolute -inset-0.5" />
-              <span className="sr-only">Open main menu</span>
-              <Bars3Icon aria-hidden="true" className="block size-6 group-data-open:hidden" />
-              <XMarkIcon aria-hidden="true" className="hidden size-6 group-data-open:block" />
-            </DisclosureButton>
-          </div>
-          <div className="flex flex-1 items-center justify-center sm:items-stretch sm:justify-start">
-            <div className="flex shrink-0 items-center text-white text-xl font-bold">
-              Matthew En
-            </div>
-            <div className="hidden sm:ml-6 sm:block">
-              <div className="flex space-x-4">
-                {navigation.map((item) => (
-                  <a
-                    key={item.name}
-                    href={item.href}
-                    className={classNames(
-                      'text-gray-300 hover:bg-gray-700 hover:text-white',
-                      'rounded-md px-3 py-2 text-sm font-medium'
-                    )}
-                  >
-                    {item.name}
-                  </a>
-                ))}
-              </div>
-            </div>
-          </div>
+      <div className="container mx-auto flex justify-between items-center">
+        <div className="ml-4">
+          <h2
+            className="text-white text-base font-medium"
+            style={{ fontFamily: '"Merriweather", serif' }}
+          >
+            Matthew En
+          </h2>
+        </div>
+        <div className='ml-4'>
+          <a
+            href="#projects"
+            className="text-white text-base px-3 font-medium"
+            style={{ fontFamily: '"Merriweather", serif' }}
+          >
+            Projects
+          </a>
+        </div>
+        <div className="ml-4">
+          <a
+            href="https://drive.google.com/file/d/1ltXc6CbdqCk--ctWWygkw-YYVQXiEgKP/view?usp=sharing"
+            className="text-white text-base px-3 font-medium"
+            style={{ fontFamily: '"Merriweather", serif' }}
+          >
+            Resume
+          </a>
         </div>
       </div>
-      <DisclosurePanel className="sm:hidden">
-        <div className="space-y-1 px-2 pt-2 pb-3">
-          {navigation.map((item) => (
-            <DisclosureButton
-              key={item.name}
-              as="a"
-              href={item.href}
-              className={classNames(
-                'block rounded-md px-3 py-2 text-base font-medium text-gray-300 hover:bg-gray-700 hover:text-white'
-              )}
-            >
-              {item.name}
-            </DisclosureButton>
-          ))}
-        </div>
-      </DisclosurePanel>
-    </Disclosure>
   );
 };
 
