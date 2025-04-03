2bb6824d9cc63c93a6ab794d903a6b478a1cb641
Matthew En <86386184+Mcen25@users.noreply.github.com>
2025-04-03 17:02:14 -0400

    Added navbar and node package for react cli

--- 3D-Portfolio-Matt/src/App.jsx
+++ 3D-Portfolio-Matt/src/App.jsx
@@ -7,26 +7,30 @@ import { useLayoutEffect, useRef, useState } from 'react'
 import { Canvas, applyProps, useFrame } from '@react-three/fiber'
 import { PerformanceMonitor, AccumulativeShadows, RandomizedLight, Environment, Lightformer, Float, useGLTF, OrbitControls} from '@react-three/drei'
 import { LayerMaterial, Color, Depth } from 'lamina'
+import NavBar from './components/NavBar/NavBar'
 
 function App() {
   const [count, setCount] = useState(0)
   const [degraded, degrade] = useState(false)
 
   return (
-    <Canvas shadows camera={{fov: 45 }} style={{ width: '100%', height: '100%' }}>
-      <OrbitControls />
-      <spotLight position={[0, 15, 0]} angle={0.3} penumbra={1} castShadow intensity={2} shadow-bias={-0.0001} />
-      <ambientLight intensity={0.5} />
-      <RoomModel/>
-      <AccumulativeShadows position={[0, -1.16, 0]} frames={100} alphaTest={0.9} scale={10}>
-        <RandomizedLight amount={8} radius={10} ambient={0.5} position={[1, 5, -1]} />
-      </AccumulativeShadows>
+    <>
+      <NavBar />
+      {/* <Canvas shadows camera={{fov: 45 }} style={{ width: '100vw', height: '100vh' }}>
+        <OrbitControls />
+        <spotLight position={[0, 15, 0]} angle={0.3} penumbra={1} castShadow intensity={2} shadow-bias={-0.0001} />
+        <ambientLight intensity={0.5} />
+        <RoomModel/>
+        <AccumulativeShadows position={[0, -1.16, 0]} frames={100} alphaTest={0.9} scale={10}>
+          <RandomizedLight amount={8} radius={10} ambient={0.5} position={[1, 5, -1]} />
+        </AccumulativeShadows>
 
-      <PerformanceMonitor onDecline={() => degrade(true)} />
-      {/* <Environment frames={degraded ? 1 : Infinity} resolution={256} background blur={1}>
-      </Environment> */}
-      {/* <CameraRig /> */}
-    </Canvas>
+        <PerformanceMonitor onDecline={() => degrade(true)} />
+        {/* <Environment frames={degraded ? 1 : Infinity} resolution={256} background blur={1}>
+        </Environment> */}
+        {/* <CameraRig /> */}
+      {/* </Canvas> */}
+    </>
   )
 }
 
