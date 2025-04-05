3f7a976b4f0c431b0984d7bc7d473794a1ea165c
Matthew En <86386184+Mcen25@users.noreply.github.com>
2025-04-05 10:36:27 -0400

    Added intro page

--- 3D-Portfolio-Matt/src/App.jsx
+++ 3D-Portfolio-Matt/src/App.jsx
@@ -1,35 +1,18 @@
 import './App.css'
-
-import * as THREE from 'three'
-import { useLayoutEffect, useRef, useState } from 'react'
-
 import NavBar from './components/NavBar/NavBar'
 import Footer from './components/Footer/Footer'
 import ThreeBox from './components/ThreeBox/ThreeBox'
 
 function App() {
-  const [count, setCount] = useState(0)
-  const [degraded, degrade] = useState(false)
 
   return (
     <>
       <NavBar />
       <ThreeBox />
+      
       <Footer />
     </>
   )
 }
-
-function RoomModel(props) {
-  const { scene, nodes, materials } = useGLTF('/RealisticRoom.glb')
-  return <primitive object={scene} {...props} />
-}
-
-// function CameraRig({ v = new THREE.Vector3() }) {
-//   return useFrame((state) => {
-//     const t = state.clock.elapsedTime
-//     state.camera.lookAt(0, 0, 0)
-//   })
-// }
 
 export default App
---
+++ 3D-Portfolio-Matt/src/components/Intro/Intro.tsx
@@ -0,0 +1,12 @@
+import React, { FC } from 'react';
+import './Intro.css';
+
+interface IntroProps {}
+
+const Intro: FC<IntroProps> = () => (
+  <div className="Intro">
+    Intro Component
+  </div>
+);
+
+export default Intro;
