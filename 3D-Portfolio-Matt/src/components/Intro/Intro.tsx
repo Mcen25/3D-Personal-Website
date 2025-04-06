b1e2e64ad2d6ecb99e55fb8c6db67d51051c7bcb
Matthew En <86386184+Mcen25@users.noreply.github.com>
2025-04-05 22:22:43 -0400

    Intro with proper text and formatting

--- 3D-Portfolio-Matt/src/components/Intro/Intro.tsx
+++ 3D-Portfolio-Matt/src/components/Intro/Intro.tsx
@@ -3,42 +3,67 @@ import './Intro.css';
 
 interface IntroProps {}
 
+const observer = new IntersectionObserver((entries) => {
+  entries.forEach((entry) => {
+    console.log(entry)
+    if (entry.isIntersecting) {
+      entry.target.classList.add('Show');
+    } else {
+      entry.target.classList.remove('Show');
+    }
+  })  
+})
+
+const hiddenElements = document.querySelectorAll('.hidden');
+hiddenElements.forEach((el) => observer.observe(el))
+
 const Intro: FC<IntroProps> = () => (
   <div className="">
-    {/* <div className="Intro flex flex-row justify-center items-center gap-x-4 md:gap-x-10 lg:gap-x-100 mt-50">
-      <div className="text-left">
-        <a>About</a>
-        <p className="mt-5">
-          Testestetsetsetsetset
+    <div className="container mx-auto flex justify-between items-start mt-50 ">
+      <div className="ml-4 text-left">
+        <h2
+          className="text-white text-base font-medium"
+          style={{ fontFamily: '"Merriweather", serif' }}
+        >
+          About
+        </h2>
+        <p className="mt-2">
+          Hi, I'm Matthew En---a newly graduated computer science student at the University of Massachusetts Amherst with a passion of building creative solutions.
         </p>
       </div>
-      <div className="text-left">
-        <p></p>
-      </div>
-      <div className="text-left">
-        <p></p>
-      </div>
-    </div> */}
-
-    <div className="container mx-auto flex justify-between items-center mt-50">
-        <div className="ml-4 text-left">
-          <h2
-            className="text-white text-base font-medium"
-            style={{ fontFamily: '"Merriweather", serif' }}
-          >
-            About
-          </h2>
-          <p>
-          Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.
-          </p>
-        </div>
-        <div className="ml-4">
-          <h2>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</h2>
-        </div>
-        <div className="ml-4 text-right">  
-          <h2> Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</h2>
+      <div className="ml-4 text-left">
+        <h2
+          className="text-white text-base font-medium"
+          style={{ fontFamily: '"Merriweather", serif' }}
+        >
+          Experience
+        </h2>
+        <div className="space-y-2 mt-2">
+          <h3 className="text-white">
+            Research Assistant @DARoS Lab at the University of Massachusetts Amherst - Feburary 2025 - Present
+          </h3>
+          <h3 className="text-white">
+            Undergraduate Research Volunteer - December 2024 - Feburary 2025
+          </h3>
+          <h3 className="text-white">
+            Technical Instructor - June 2024 - August 2024
+          </h3>
         </div>
       </div>
+      <div className="ml-4 text-left">
+        <h2
+          className="text-white text-base font-medium"
+          style={{ fontFamily: '"Merriweather", serif' }}
+        >
+          Skills  
+        </h2>  
+        <div className="space-y-2 mt-2">
+          <h3 className="text-white">
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.
+          </h3>
+        </div>
+      </div>
+    </div>
   </div>
 );
 
