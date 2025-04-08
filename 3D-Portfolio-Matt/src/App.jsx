74bf4f91a292e42f71fe9d039da193cae8158e76
Matthew En <86386184+Mcen25@users.noreply.github.com>
2025-04-08 00:28:53 -0400

    Added Marquee and Bento Grid

--- 3D-Portfolio-Matt/src/App.jsx
+++ 3D-Portfolio-Matt/src/App.jsx
@@ -3,13 +3,17 @@ import NavBar from './components/NavBar/NavBar'
 import Footer from './components/Footer/Footer'
 import ThreeBox from './components/ThreeBox/ThreeBox'
 import Intro from './components/Intro/Intro'
+import BentoGrid from './components/BentoGrid/BentoGrid'
+import Marquee from './components/Marquee/Marquee'
 function App() {
 
   return (
     <>
       <NavBar />
+      <Marquee />
       <ThreeBox />
       <Intro />
+      <BentoGrid />
       <Footer />
     </>
   )
