a4433737fd2ed2e29d12be54b98e82a7540d8875
Matthew En <86386184+Mcen25@users.noreply.github.com>
2025-04-29 02:19:45 -0400

    Added spotlight, performance a little laggy

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -3,16 +3,20 @@ import NavBar from "./components/NavBar/NavBar";
 import Footer from "./components/Footer/Footer";
 import ThreeBox from "./components/ThreeBox/ThreeBox";
 import Intro from "./components/Intro/Intro";
-import BentoGrid from "./components/BentoGrid/BentoGrid";
 import Marquee from "./components/Marquee/Marquee";
-import Skills from "./components/Skills/Skills";
 
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import DGA from "./pages/DGA";
 import TamaTask from "./pages/TamaTask";
 import ResearchAssistant from "./pages/ResearchAssistant";
+import { Spotlight } from "./components/ui/spotlight-new";
+import React, { Suspense, lazy } from "react";
+
 
 function App() {
+  const BentoGrid = lazy(() => import("./components/BentoGrid/BentoGrid.jsx"));
+  const Skills = lazy(() => import("./components/Skills/Skills.tsx"));
+
   return (
     <Router>
       <Routes>
@@ -21,11 +25,23 @@ function App() {
           element={
             <>
               <NavBar />
-              <Marquee />
-              <ThreeBox />
+              {/* <Marquee /> */}
+              <div className=" p-2 max-w-7xl  mx-auto relative z-10 mt-40 w-full pt-20 md:pt-0">
+                <Spotlight /> 
+                <h1 className="text-4xl md:text-7xl font-bold text-center text-white">
+                  Matthew En <br />
+                </h1>
+                <p className="mt-4 font-normal text-base text-neutral-300 max-w-lg text-center mx-auto">
+                Aspiring software engineer, passionate full-stack developer, and lifelong learner.
+                </p>
+              </div>
               <Intro />
-              <Skills />
-              <BentoGrid />
+              <Suspense fallback={<div>Loading...</div>}>
+                <Skills />
+              </Suspense>
+              <Suspense fallback={<div>Loading...</div>}>
+                <BentoGrid />
+              </Suspense>
               <Footer />
             </>
           }
